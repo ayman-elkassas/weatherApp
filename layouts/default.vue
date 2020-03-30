029c1785d1f76aa59126f8899ae047abefb5e40f
Ayman Elkassas <aymanelkassas88@gmail.com>
2020-03-31 01:14:57 +0200

    commit

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,28 +1,28 @@
 <template>
-  <div>
-    <v-toolbar extension-height="">
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-app>
+      <v-content>
+        <v-toolbar dark color="indigo">
+          <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-      <v-toolbar-title>{{title}}</v-toolbar-title>
+          <v-toolbar-title>{{title}}</v-toolbar-title>
 
-      <v-spacer></v-spacer>
+          <v-spacer></v-spacer>
 
-      <v-btn icon>
-        <n-link to="/"><v-icon>mdi-home</v-icon></n-link>
-      </v-btn>
+          <v-toolbar-items class="hidden-sm-and-down">
 
-      <v-btn icon>
-        <span><n-link to="/about"><v-icon>mdi-dots-horizontal</v-icon></n-link></span>
-      </v-btn>
+            <v-btn style="background: none">
+              <n-link to="/weather-app">Weather App</n-link>
+            </v-btn>
 
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
-    </v-toolbar>
+          </v-toolbar-items>
+        </v-toolbar>
+        <transition>
+          <nuxt></nuxt>
+        </transition>
 
-    <nuxt></nuxt>
-  </div>
+      </v-content>
 
+    </v-app>
 
 </template>
 
@@ -30,35 +30,16 @@
 export default {
   data:function() {
     return {
-      title:'Nuxt List'
+      title:'Weather App'
     }
   }
-  // data () {
-  //   return {
-  //     clipped: false,
-  //     drawer: false,
-  //     fixed: false,
-  //     items: [
-  //       {
-  //         icon: 'mdi-apps',
-  //         title: 'Welcome',
-  //         to: '/'
-  //       },
-  //       {
-  //         icon: 'mdi-chart-bubble',
-  //         title: 'Inspire',
-  //         to: '/inspire'
-  //       }
-  //     ],
-  //     miniVariant: false,
-  //     right: true,
-  //     rightDrawer: false,
-  //     title: 'Vuetify.js'
-  //   }
-  // }
 }
 </script>
 
 <style scoped>
+  a{
+    text-decoration: none;
+    color: white;
 
+  }
 </style>
--- pages/weather-app.vue
+++ pages/weather-app.vue
@@ -1,10 +1,60 @@
 <template>
-    
+    <v-container>
+<!--      <v-layout>-->
+      <h1 class="display-1 text-xs-center">Weather App</h1>
+        <v-flex xs12>
+          <v-card color="blue-gray darken-2" dark>
+            <v-card-text>
+              <v-layout v-if="weather.weather" justify-center>
+                <v-flex class="text-xs-center">
+                  <h4>Temperature</h4>
+                  <h1 class="display-1">{{weather.name}}</h1>
+                  <img :src="weather.weather[0].icon" alt="weather icon">
+                </v-flex>
+              </v-layout>
+            </v-card-text>
+          </v-card>
+        </v-flex>
+
+<!--      //TODO: enter after write city in field to go request-->
+      <form @submit.prevent="getWeatherInfo">
+        <v-flex xs12 class="mt-4">
+          <v-text-field solo label="Enter City Name" v-model="city"></v-text-field>
+        </v-flex>
+      </form>
+
+      <div>
+        {{this.weather}}
+      </div>
+
+<!--      </v-layout>-->
+    </v-container>
 </template>
 
 <script>
   export default {
-    name : 'weather-app'
+    name : 'weather-app',
+    data(){
+      return{
+        city:'London',
+        weather:{}
+      }
+    },
+    created(){
+      this.getWeatherInfo();
+    },
+    methods:{
+      getWeatherInfo(){
+        this.$axios.$get(
+          'https://api.openweathermap.org/data/2.5/weather?q=' +
+          this.city+
+          '&appid=913917a63abeb8f987f2a74faa0615c8'
+        ).then(res=>{
+          console.log(res)
+          this.weather=res
+        })
+      }
+    }
   }
 </script>
 
